641f998964e860d82879f50e9b84c751d22b3b19
Elay Ben Yehoshua <elaybencp3@gmail.com>
2024-09-13 19:30:49 +0300

    move the logic to card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
 import './App.css';
+import { useState } from 'react';
+import SingleCard from './components/SingleCard';
+
+const cardsImages = [
+  { "src": "/pics/2_of_hearts.png" },
+  { "src": "/pics/3_of_clubs.png" },
+  { "src": "/pics/4_of_diamonds.png" },
+  { "src": "/pics/5_of_clubs.png" },
+  { "src": "/pics/6_of_clubs.png" },
+  { "src": "/pics/7_of_spades.png" },
+  { "src": "/pics/8_of_diamonds.png" },
+  { "src": "/pics/9_of_diamonds.png" },
+  { "src": "/pics/10_of_spades.png" },
+  { "src": "/pics/ace_of_diamonds.png" },
+  { "src": "/pics/black_joker.png" },
+  { "src": "/pics/jack_of_diamonds2.png" },
+  { "src": "/pics/king_of_diamonds2.png" },
+  { "src": "/pics/queen_of_diamonds2.png" }
+];
 
 function App() {
+
+
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+
+  // shuffle the cards
+  const shuffleCards = () => {
+    const shuffleCards = [...cardsImages, ...cardsImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffleCards);
+    setTurns(0);
+  }
+
+  console.log(cards, turns);
+
   return (
     <div className="App">
       <h1> Memory Rush </h1>
-      <button> Start Game </button>
+      <button onClick={shuffleCards}> Start Game </button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard key={card.id} card={card} />
+        ))}
+      </div>
     </div>
   );
 }
